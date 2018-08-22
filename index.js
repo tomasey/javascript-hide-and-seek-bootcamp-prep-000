8aa4a526f562d2ddcc318c8c992a5c9a1cf9fcae
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-22 06:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(target) {
+  return document.getElementsById('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var rank = document.querySelector('#app').querySelectorAll("ul.ranked-list li");
+  for (i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n
+  }  
+}
