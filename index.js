ab0fac0cf4abb39157e101f3084ba7588b45bc38
oschoyzam@gmail.com <oschoyzam@gmail.com>
2018-08-22 07:05:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(target) {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var rank = document.querySelector('#app').querySelectorAll("ul.ranked-list li");
+  for (var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n
+  }  
+}
+
+function deepestChild() {
+  var child = document.getElementById('grand-node').querySelectorAll('div');
+  return child[child.length - 1]
+}
